7fda11f5cf19092ef43f78a127908f37d4bbbb24
Fabio Privitera <fabio_privitera@yahoo.it>
2022-03-14 13:46:21 +0200

    add axios, async to fetch data from unsplashed API, state to contain array of images from API

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import axios from 'axios';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
-  }
+  state = { images: [] };
+  onSearchSubmit = async (term) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID Rdo1CRpIk_Is6we_uArk0akPH6VTw2LiwhE__djwI74',
+      },
+    });
+    this.setState({ images: response.data.results });
+  };
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubbbmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
