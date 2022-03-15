9618fbd07f62141643d3a12eb551d25b1b4e898e
Fabio Privitera <fabio_privitera@yahoo.it>
2022-03-15 10:56:00 +0200

    React Ref, dynamic spans, new component: ImageCard.js, ImageList.js

--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './ImageList.css';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
   const images = props.images.map((image) => {
-    return <img key={image.id} src={image.urls.regular}></img>;
+    return <ImageCard key={image.id} image={image} />;
   });
-  return <div key={images.id}>{images}</div>;
+  return (
+    <div className="image-list" key={images.id}>
+      {images}
+    </div>
+  );
 };
 export default ImageList;
